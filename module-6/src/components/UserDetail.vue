32ca10c9e65aee82a7b744811cd61a91b370a2ff
Nikola Obradovic <obradovicnik01@gmail.com>
2019-09-11 16:10:28 +0200

    Props

---
+++ module-6/src/components/UserDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{switchName()}}</p>
+    <button @click="resetName">Reset Name</button>
+    <button @click="resetFn()">Reset Name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String
+    },
+    resetFn: Function
+  },
+  methods: {
+    switchName() {
+      return this.name
+        .split("")
+        .reverse()
+        .join("");
+    },
+    resetName() {
+      this.name = "Nikola";
+      this.$emit("nameWasReset", this.name);
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightcoral;
+}
+</style>

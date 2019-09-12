52a59e68f00a693a1ba3229e074a4e733147dfa2
Nikola Obradovic <obradovicnik01@gmail.com>
2019-09-12 17:01:40 +0200

    ...

--- module-6/src/components/UserDetail.vue
+++ module-6/src/components/UserDetail.vue
@@ -3,18 +3,21 @@
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
     <p>User Name: {{switchName()}}</p>
+    <p>User Age: {{userAge}}</p>
     <button @click="resetName">Reset Name</button>
     <button @click="resetFn()">Reset Name</button>
   </div>
 </template>
 
 <script>
+import { eventBus } from "../main";
 export default {
   props: {
     name: {
       type: String
     },
-    resetFn: Function
+    resetFn: Function,
+    userAge: Number
   },
   methods: {
     switchName() {
@@ -27,6 +30,11 @@ export default {
       this.name = "Nikola";
       this.$emit("nameWasReset", this.name);
     }
+  },
+  created() {
+    eventBus.$on("ageWasEdited", age => {
+      this.userAge = age;
+    });
   }
 };
 </script>

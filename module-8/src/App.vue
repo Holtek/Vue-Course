694b395af5ea4d9d87e961da7f2af0111b94e8a9
Nikola Obradovic <obradovicnik01@gmail.com>
2019-09-14 15:56:56 +0200

    2nd project donzo :)

---
+++ module-8/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <app-progress :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-progress>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a quote to delete it</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Progress from "./components/Progress.vue";
+export default {
+  data: function() {
+    return {
+      quotes: ["Just a Quote to see something"],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete quotes first");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appProgress: Progress
+  }
+};
+</script>
+
+<style>
+</style>

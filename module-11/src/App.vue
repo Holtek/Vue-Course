ec109091991c6ccb1d274f7179415d11ad300073
Nikola Obradovic <obradovicnik01@gmail.com>
2019-09-18 18:19:43 +0200

    mixins and filters

---
+++ module-11/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <p>{{ text | toUpperCase | to-lower-case }}</p>
+        <hr />
+        <button @click="fruits.push('berries')"></button>
+        <input v-model="filterText " />
+        <ul>
+          <li v-for="fruit in filteredFruits">{{fruit}}</li>
+        </ul>
+        <hr />
+        <app-list></app-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { fruitMixin } from "./Mixin.js";
+import List from "./List.vue";
+export default {
+  data() {
+    return {
+      text: "Hello There"
+    };
+  },
+  filters: {
+    toUpperCase(value) {
+      return value.toUpperCase();
+    }
+  },
+  components: {
+    appList: List
+  },
+  mixins: [fruitMixin]
+};
+</script>
+
+<style>
+</style>
---
+++ module-11/src/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Filters & Mixins</h1>
+    <input v-model="filterText" />
+    <ul>
+      <li v-for="fruit in filteredFruits">{{fruit}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { fruitMixin } from "./Mixin.js";
+export default {
+  mixins: [fruitMixin],
+  created() {
+    console.log("inside list created hook");
+  }
+};
+</script>
+
+<style>
+</style>

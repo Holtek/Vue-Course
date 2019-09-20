b6b0d608b88fe767ed1639132d2e601bf58a1226
Nikola Obradovic <obradovicnik01@gmail.com>
2019-09-20 18:08:50 +0200

    More Vue Animations

--- module-12/src/App.vue
+++ module-12/src/App.vue
@@ -4,17 +4,59 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
         <hr />
+        <select v-model="alertAnimation" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br />
         <button class="btn btn-primary" @click="show = !show">Show Alert</button>
         <br />
         <br />
-        <transition name="fade">
+
+        <transition :name="alertAnimation">
           <div class="alert alert-info" v-show="show">This is some info</div>
         </transition>
-        <transition name="slide" type="animation">
+
+        <transition :name="alertAnimation" type="animation" appear>
           <div class="alert alert-info" v-if="show">This is some info</div>
         </transition>
-        <transition name="fade" appear>
+
+        <transition enter-active-class="animated bounce" leave-active-class="animated shake">
           <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+
+        <transition :name="alertAnimation" mode="out-in">
+          <div class="alert alert-info" v-if="show" key="info">This is some info</div>
+          <div class="alert alert-warning" v-else key="warning">This is some Warning</div>
+        </transition>
+
+        <hr />
+        <br />
+
+        <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+        <br />
+        <br />
+        <transition
+          @before-enter="beforeEnter"
+          @enter="enter"
+          @after-enter="afterEnter"
+          @enter-cancelled="enterCancelled"
+          @before-leave="beforeLeave"
+          @leave="leave"
+          @after-leave="afterLeave"
+          @leave-cancelled="leaveCancelled"
+          :css="false"
+        >
+          <div style="width: 300px; height: 100px; background: lightgreen" v-if="load"></div>
+        </transition>
+        <hr />
+        <button
+          class="btn btn-primary"
+          @click="selectedComponent === 'app-success-alert' ? selectedComponent = 'app-danger-alert' : selectedComponent = 'app-success-alert'"
+        >Toggle Components</button>
+        <br />
+        <transition name="fade" mode="out-in">
+          <component :is="selectedComponent"></component>
         </transition>
       </div>
     </div>
@@ -22,11 +64,70 @@
 </template>
 
 <script>
+import DangerAlert from "./DangerAlert.vue";
+import SuccessAlert from "./SuccessAlert.vue";
 export default {
   data() {
     return {
-      show: true
+      show: false,
+      load: true,
+      alertAnimation: "fade",
+      elementWidth: 100,
+      selectedComponent: "app-success-alert"
     };
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log("beforeEnter");
+      this.elementWidth = 100;
+      el.style.width = this.elementWidth + "px";
+    },
+    enter(el, done) {
+      console.log("enter");
+      let round = 1;
+      const interval = setInterval(() => {
+        el.style.width = this.elementWidth + round * 10 + "px";
+        round++;
+        if (round > 20) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+      done();
+    },
+    afterEnter(el) {
+      console.log("afterEnter");
+    },
+    enterCancelled(el) {
+      console.log("enterCancelled");
+    },
+    beforeLeave(el) {
+      console.log("beforeLeave");
+      this.elementWidth = 300;
+      el.style.width = this.elementWidth + "px";
+    },
+    leave(el, done) {
+      console.log("leave");
+      let round = 1;
+      const interval = setInterval(() => {
+        el.style.width = this.elementWidth - round * 10 + "px";
+        round++;
+        if (round > 20) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+    },
+    afterLeave(el) {
+      console.log("afterLeave");
+    },
+    leaveCancelled(el) {
+      console.log("leaveCancelled");
+    }
+  },
+  components: {
+    appDangerAlert: DangerAlert,
+    appSuccessAlert: SuccessAlert
   }
 };
 </script>
